cbb3201fe610b3b71346e62c20304ded36b69fde
pujariakash <pujariakash653@gmail.com>
2025-02-15 16:31:16 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// accesesing class and id //
 const display= document.querySelector(".screen");
 const buttons= document.querySelectorAll('.btn');
 const equalButton=document.querySelector('.equal');
@@ -5,19 +6,21 @@ const clearbutton=document.querySelector('.clear');
 const deletebutton=document.querySelector('.del');
 const bracket=document.querySelector(".bracket")
 
-
+//adding event listener for all button//
 buttons.forEach((btn)=>{
     btn.addEventListener('click',()=>{
         display.innerText +=btn.innerText;
     })
 });
-
+//clearing display after click on c or clear class//
 clearbutton.addEventListener('click',()=>{
     display.textContent="";
 })
+//deleting button//
 deletebutton.addEventListener('click',()=>{
     display.textContent=display.textContent.slice(0,-1);
 })
+//uses eval builtin function//
 equalButton.addEventListener('click',()=>{
     try{ display.innerText=eval(display.innerText);}
     catch(error){
